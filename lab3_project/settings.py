67962b148b7cd614ee28f2b103e835ce51f2b175
solunska <a_solunska@outlook.com>
2024-01-19 14:07:15 +0100

    changes to settings.py

--- lab3_project/settings.py
+++ lab3_project/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'lab3_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("POSTGRES_NAME"),
-        'USER': os.environ.get("POSTGRES_USER"),
+        'NAME': 'djangoPostsDB',
+        'USER': 'postgres',
         'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
-        'HOST': os.environ.get("POSTGRES_HOST"),
-        'PORT': os.environ.get("POSTGRES_PORT"),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
